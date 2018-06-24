78310479badea5ba5fd2a28833eee9cdfd263a04
LatyninEugene <31857296+LatyninEugene@users.noreply.github.com>
2018-06-24 17:24:39 +0300

    aa

--- src/main/java/view/LoginServlet.java
+++ src/main/java/view/LoginServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "login")
+@WebServlet(urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
     @Inject
